24cf9db7cfe5c51707db5ad5967635c94f2eaa07
tsukumi <tsukumijima@users.noreply.github.com>
2022-04-15 05:59:40 +0900

    Add feature to display access log.

--- https-server/main.go
+++ https-server/main.go
@@ -6,9 +6,17 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+	"os/signal"
+
+	"github.com/fatih/color"
 )
 
 func main() {
+
+	// log prefix (escape sequence)
+	var infoLogPrefix = color.GreenString("Info") + ": "
+	var errorLogPrefix = color.RedString("Error") + ":"
 
 	// load config data
 	if err := loadConfig(); err != nil {
@@ -16,10 +24,39 @@ func main() {
 	}
 
 	// setup reverse proxy
-	var url, _ = url.Parse(config.ProxyPassURL)
-	var reverseProxy = http.Server{
+	var proxyPassURL, _ = url.Parse(config.ProxyPassURL)
+	var reverseProxy = httputil.NewSingleHostReverseProxy(proxyPassURL)
+	reverseProxy.ModifyResponse = func(response *http.Response) error {
+		// set status code color
+		var statusCodeText string
+		switch {
+		case response.StatusCode >= 200 && response.StatusCode <= 299:
+			statusCodeText = color.GreenString(response.Status)
+		case response.StatusCode >= 300 && response.StatusCode <= 399:
+			statusCodeText = color.YellowString(response.Status)
+		case response.StatusCode >= 400 && response.StatusCode <= 599:
+			statusCodeText = color.RedString(response.Status)
+		default:
+			statusCodeText = response.Status
+		}
+
+		// print access log
+		log.Printf(
+			"%s %s - \"%s %s %s\" %s",
+			infoLogPrefix,
+			response.Request.RemoteAddr,
+			response.Request.Method,
+			response.Request.URL.Path,
+			response.Request.Proto,
+			statusCodeText,
+		)
+		return nil
+	}
+
+	// setup reverse proxy server
+	var reverseProxyServer = http.Server{
 		Addr:    config.ListenAddress,
-		Handler: httputil.NewSingleHostReverseProxy(url),
+		Handler: reverseProxy,
 		// set GetCertificate callback
 		TLSConfig: &tls.Config{
 			GetCertificate: GetKeylessServerCertificate(config.KeylessServerURL),
@@ -27,8 +64,20 @@ func main() {
 	}
 
 	// serve reverse proxy
-	var err = reverseProxy.ListenAndServeTLS("", "")
-	if err != nil {
-		log.Fatalln("error:", err)
-	}
+	go func() {
+		log.Println(infoLogPrefix, "Starting HTTPS reverse proxy server...")
+		log.Printf("%s Listening on %s, Proxing %s.", infoLogPrefix, config.ListenAddress, config.ProxyPassURL)
+		var err = reverseProxyServer.ListenAndServeTLS("", "")
+		if err != nil {
+			log.Fatalln(errorLogPrefix, err)
+		}
+	}()
+
+	// set signal trap
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	// When Ctrl+C is pressed
+	<-quit
+	defer log.Println(infoLogPrefix, "Terminated HTTPS reverse proxy server.")
 }
